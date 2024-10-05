8ccbb0a69f1f26bc7caa52e570628760c663b666
Sahil Jaiswal <sahiljaiswal550@gmail.com>
2024-10-05 20:05:30 +0530

    implemented login page

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import InputForm from "../../components/input-form";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [category, setCategory] = React.useState("ORG");
+
+  return (
+    <>
+      <div className="flex justify-center items-center my-2 gap-2 font-garamond">
+        <button
+          className={`${
+            category === "ORG"
+              ? "bg-secondary p-2 rounded"
+              : "p-2 border-b-2 border-b-secondary"
+          }`}
+          onClick={() => setCategory("ORG")}
+        >
+          Organisation
+        </button>
+        <button
+          className={`${
+            category === "STUDENT"
+              ? "bg-secondary p-2 rounded"
+              : "p-2 border-b-2 border-b-secondary"
+          }`}
+          onClick={() => setCategory("STUDENT")}
+        >
+          Student
+        </button>
+      </div>
+      <div>
+        {category === "ORG" && (
+          <InputForm category={category} type={"LOGIN"} />
+        )}
+      </div>
+      <div>
+        {category === "STUDENT" && (
+          <InputForm category={category} type={"LOGIN"} />
+        )}
+      </div>
+    </>
+  );
 };
 
 export default Login;
